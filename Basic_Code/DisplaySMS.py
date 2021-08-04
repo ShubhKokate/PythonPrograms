cc1826e75a76d7d173001d58edf77b6bc0ed72b4
ShubhKokate <81962910+ShubhKokate@users.noreply.github.com>
2021-08-04 16:08:58 +0800

    Create DisplaySMS.py

---
+++ Basic_Code/DisplaySMS.py
@@ -0,0 +1,15 @@
+# Display massage on scren5 times
+#============================Called Function #======================
+def Display(Value):
+  cint=0
+  while cint < value:
+    print("Hello World!")
+    cint= cint+1
+#=============================Main Function #=========================   
+def main():
+  Num=5
+  Display(Num)
+  
+#=============================Starter #===============================  
+if __name__=="__main__":
+  main()
