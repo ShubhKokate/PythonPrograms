c706479f4eb554571f189f209c77f2975f5a7b8d
ShubhKokate <81962910+ShubhKokate@users.noreply.github.com>
2021-08-04 16:15:10 +0800

    Create DisplayNum1.py

---
+++ Basic_Code/DisplayNum1.py
@@ -0,0 +1,17 @@
+#display number from 10 to 1 on screen
+
+#=============================Called Function #===============================
+def Display():
+    cint=0
+		for cint in range(10, 0, -1):
+				print(cint)
+  
+#=============================Main Function #==================================
+def main():
+    Display()
+    
+#============================= Starter #=======================================
+if __name__=="__main__":
+    main()
+
+				
